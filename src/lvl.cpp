dc6ea1a6c09c7100eacf7656c8dccfcac11428dd
MartinOpat <martinopat.opat@gmail.com>
2024-07-13 23:32:52 +0200

    Made lvl 1 more reasonable in difficulty

--- src/lvl.cpp
+++ src/lvl.cpp
@@ -5,38 +5,30 @@ lvl::lvl(int num) {
         case 1:
             // First column
             posSushis.push_back(b2Vec2(300, 400));
-            posSushis.push_back(b2Vec2(300, 500));
-            posSushis.push_back(b2Vec2(300, 600));
             posSushis.push_back(b2Vec2(300, 700));
             posSushis.push_back(b2Vec2(300, 1000));
 
-            posObstacles.push_back(b2Vec2(350, 300));
+            posObstacles.push_back(b2Vec2(450, 300));
 
 
             // Second column
             posSushis.push_back(b2Vec2(600, 400));
-            posSushis.push_back(b2Vec2(600, 500));
-            posSushis.push_back(b2Vec2(600, 600));
             posSushis.push_back(b2Vec2(600, 700));
             posSushis.push_back(b2Vec2(600, 1000));
 
 
             // Third column
             posSushis.push_back(b2Vec2(900, 400));
-            posSushis.push_back(b2Vec2(900, 500));
-            posSushis.push_back(b2Vec2(900, 600));
             posSushis.push_back(b2Vec2(900, 700));
             posSushis.push_back(b2Vec2(900, 1000));
 
+            posObstacles.push_back(b2Vec2(1050, 300));
 
             // Fourth column
             posSushis.push_back(b2Vec2(1200, 400));
-            posSushis.push_back(b2Vec2(1200, 500));
-            posSushis.push_back(b2Vec2(1200, 600));
             posSushis.push_back(b2Vec2(1200, 700));
             posSushis.push_back(b2Vec2(1200, 1000));
 
-            posObstacles.push_back(b2Vec2(1250, 300));
 
 
             obstacleWidth = 200;
